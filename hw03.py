25a79a88456c91d721ef205ec2b4e72b872c27c7
Avery Brinkman <brinkmae@mail.uc.edu>
2021-03-26 19:44:59 -0400

    Version 1

--- hw03.py
+++ hw03.py
@@ -9,3 +9,16 @@ from urllib.request import urlopen
 wordfile = urlopen(url)
 words = wordfile.read().decode('utf-8').upper().split()
 
+def step(inputWord):
+    print("Running function...")
+    checkWords = []
+    outWords = []
+    for i in range(65, 91):
+        checkWords.append(inputWord + chr(i))
+    for w in words:
+        for cW in checkWords:
+            if sorted(w) == sorted(cW):
+                outWords += [w]
+    return outWords
+
+print(step("APPLE"))
