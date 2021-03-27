c3bb77a6e7ff69bfc4ab09821d50326610ff960b
Avery Brinkman <brinkmae@mail.uc.edu>
2021-03-26 20:13:04 -0400

    Version 3

--- hw03.py
+++ hw03.py
@@ -2,6 +2,8 @@ _author_ = "Avery Brinkman"
 _credits_ = []
 _email_ = "brinkmae@mail.uc.edu"
 
+import time
+
 # Provided code
 url = "http://raw.githubusercontent.com/eneko/data-repository/master/data/words.txt"
 import os
@@ -9,18 +11,24 @@ from urllib.request import urlopen
 wordfile = urlopen(url)
 words = wordfile.read().decode('utf-8').upper().split()
 
+checkWords = []
+for i in range(65, 91):
+    checkWords.append(chr(i))
+
 def step(inputWord):
     print("Running function...")
-    checkWords = []
+    start_time = time.time()
+
     outWords = []
-    for i in range(65, 91):
-        checkWords.append(inputWord + chr(i))
     for w in words:
         if len(w) != len(inputWord)+1:
             continue
         for cW in checkWords:
-            if sorted(w) == sorted(cW):
+            if sorted(w) == sorted(inputWord + cW):
                 outWords += [w]
+
+    print("runtime:", time.time()-start_time)
     return outWords
+
 
 print(step("APPLE"))
